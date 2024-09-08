e9d36335021aac17bb4b7c1c7b8e2311681c4bac
karlchvojka <karl.chvojka@gmail.com>
2024-09-08 19:29:48 -0400

    IMP: FCC/JS/Pyramid_gen: Lesson 45

--- FreeCodeCamp/JS_Algor_Data_Structures/01_Pyramid_Gen/script.js
+++ FreeCodeCamp/JS_Algor_Data_Structures/01_Pyramid_Gen/script.js
@@ -2,7 +2,7 @@ const character = "#";
 const count = 8;
 const rows = [];
 for(let i = 0; i < count; i = i + 1) {
-    rows.push(character);
+    rows.push(character.repeat(i));
 }
 let result = "";
 for (const row of rows){
