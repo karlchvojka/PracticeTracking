783528f21ab8bfe8e57cc88dfa1ca16d372efea3
karlchvojka <karl.chvojka@gmail.com>
2024-09-08 17:00:50 -0400

    IMP: FCC/JS/Pyramid_gen: Lesson 24

--- FreeCodeCamp/JS_Algor_Data_Structures/01_Pyramid_Gen/script.js
+++ FreeCodeCamp/JS_Algor_Data_Structures/01_Pyramid_Gen/script.js
@@ -1,4 +1,7 @@
 let character = "Hello";
 let count = 8;
 let rows = ["Naomi", "Quincy", "CamperChan"];
-console.log(rows);
+let cities = ["London", "New York", "Mumbai"];
+console.log(cities);
+cities[cities.length - 1] = "Mexico City";
+console.log(cities);
