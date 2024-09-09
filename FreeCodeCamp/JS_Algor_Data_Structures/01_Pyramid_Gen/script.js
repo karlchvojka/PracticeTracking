3e4d7410980ec98a1fe33047ffdb64a7df7a043f
karlchvojka <karl.chvojka@gmail.com>
2024-09-08 23:10:24 -0400

    IMP: FCC/JS/Pyramid_gen: Lesson 109

--- FreeCodeCamp/JS_Algor_Data_Structures/01_Pyramid_Gen/script.js
+++ FreeCodeCamp/JS_Algor_Data_Structures/01_Pyramid_Gen/script.js
@@ -45,13 +45,6 @@ for (let i = count; i > 0; i--) {
 }
 */
 
-const numbers = [1, 2, 3];
-const shifted = numbers.shift();
-console.log(shifted);
-const unshifted = numbers.unshift(5);
-console.log(unshifted);
-console.log(numbers);
-
 let result = "";
 
 for (const row of rows){
