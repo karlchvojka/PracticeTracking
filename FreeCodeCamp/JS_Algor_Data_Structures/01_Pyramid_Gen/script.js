7f7ed85b575230809b19ecd69cdf604e9c42550e
karlchvojka <karl.chvojka@gmail.com>
2024-09-08 21:22:43 -0400

    IMP: FCC/JS/Pyramid_gen: Lesson 59
    Implemented basic JSDocs comments

--- FreeCodeCamp/JS_Algor_Data_Structures/01_Pyramid_Gen/script.js
+++ FreeCodeCamp/JS_Algor_Data_Structures/01_Pyramid_Gen/script.js
@@ -1,16 +1,32 @@
+/**
+ * Pyramid Generator - FCC Project
+ * 
+ * Generates a pyramid of characters.
+ * @function
+ * @param {string} character Character to be used within the Generator
+ * @param {number} count How many rows the Pyramid will have
+ * @param {array} rows Variable to hold the rows of the pyramid
+ * @param {string} result String to be printed in the console
+ * @returns {string}
+ */
+// Variable Declarations:
 const character = "#";
 const count = 8;
 const rows = [];
+
 function padRow(name) {
-    const test = Testing;
-    return character  + name;
+    const test = "Testing";
+    return test;
 }
 const call = padRow("CamperChan");
 console.log(call);
+
 for(let i = 0; i < count; i = i + 1) {
     rows.push(character.repeat(i + 1));
 }
+
 let result = "";
+
 for (const row of rows){
     result = result + "\n" + row;
 }
