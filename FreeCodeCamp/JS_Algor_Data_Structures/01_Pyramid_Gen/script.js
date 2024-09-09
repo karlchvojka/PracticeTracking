9021109e329af4ea897034294952049f68908933
karlchvojka <karl.chvojka@gmail.com>
2024-09-08 23:27:02 -0400

    IMP: FCC/JS/Pyramid_gen: Lesson 119

--- FreeCodeCamp/JS_Algor_Data_Structures/01_Pyramid_Gen/script.js
+++ FreeCodeCamp/JS_Algor_Data_Structures/01_Pyramid_Gen/script.js
@@ -6,14 +6,15 @@
  * @param {string} character Character to be used within the Generator
  * @param {number} count How many rows the Pyramid will have
  * @param {boolean} inverted Assigns if the Pyramid should be generated upside down
+ * @param {string} result String to be printed in the console.
  * @param {array} rows Variable to hold the rows of the pyramid
- * @param {string} result String to be printed in the console
  * @returns {string}
  */
 // Variable Declarations:
 const character = "!";
 const count = 10;
 let inverted = false;
+let result = "";
 const rows = [];
 
 /**
@@ -29,6 +30,11 @@ function padRow(rowNumber, rowCount) {
     return " ".repeat(rowCount - rowNumber) + character.repeat(2 * rowNumber - 1) + " ".repeat(rowCount - rowNumber);
 }
 
+/**
+ * Iterate 'count' number of times.
+ * If Inverted is true, add new lines to the top of the pyramid.
+ * Else, add lines to the bottom (normal)
+*/
 for(let i = 1; i <= count; i++) {
     if (inverted) {
         rows.unshift(padRow(i, count));
@@ -37,9 +43,13 @@ for(let i = 1; i <= count; i++) {
     }
 }
 
-let result = "";
-
+/**
+ * Iterate over Rows array.
+ * For each, add a new row to the results variable.
+*/
 for (const row of rows){
     result = result + "\n" + row;
 }
+
+// Print final result to console.
 console.log(result);
