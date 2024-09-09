9793dd729eb7e84cea1a531c11607b36395842fd
karlchvojka <karl.chvojka@gmail.com>
2024-09-08 22:15:37 -0400

    IMP: FCC/JS/Pyramid_gen: Lesson 77

--- FreeCodeCamp/JS_Algor_Data_Structures/01_Pyramid_Gen/script.js
+++ FreeCodeCamp/JS_Algor_Data_Structures/01_Pyramid_Gen/script.js
@@ -27,9 +27,12 @@ function padRow(rowNumber, rowCount) {
     return " ".repeat(rowCount - rowNumber) + character.repeat(2 * rowNumber - 1) + " ".repeat(rowCount - rowNumber);
 }
 
+// TODO: use a different type of loop
+/*
 for(let i = 1; i <= count; i++) {
     rows.push(padRow(i, count));
 }
+*/
 
 let result = "";
 
