1efcf5c8e06fef5bcda8b833ae6341fba8efb0bc
karlchvojka <karl.chvojka@gmail.com>
2024-09-09 23:26:08 -0400

    IMP: FCC/JS/02_Gradebook: Lesson 1

--- FreeCodeCamp/JS_Algor_Data_Structures/02_Gradebook/script.js
+++ FreeCodeCamp/JS_Algor_Data_Structures/02_Gradebook/script.js
@@ -1,4 +1,25 @@
+/**
+ * Average Calculator - FCC Project
+ *
+ * Returns the average of an array of numbers
+ * @function
+ * @param {array} scores An array of numbers.
+ * @returns {number}
+ */
 function getAverage(scores) {
+    // Variable declarations
+    let average = 0;
+    let total = 0;
+
+    // Sum numbers from entered array.
+    for (const score of scores) {
+        total = total + score;
+    }
+    // Calculate the average.
+    average = total / scores.length;
+
+    // Return average
+    return average;
 }
 
 console.log(getAverage([92, 88, 12, 77, 57, 100, 67, 38, 97, 89]));
