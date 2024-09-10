b02f797550ce529b4c12f9a17634673115e0bc69
karlchvojka <karl.chvojka@gmail.com>
2024-09-10 13:09:53 -0400

    IMP: FCC/JS/03_RPGame: Lesson 29

--- FreeCodeCamp/JS_Algor_Data_Structures/03_RPGame/script.js
+++ FreeCodeCamp/JS_Algor_Data_Structures/03_RPGame/script.js
@@ -1,15 +1,27 @@
-// Interface Variables:
+/* --- INTERFACE VARIABLES --- */
+// Interactable Elements
 const button1 = document.querySelector("#button1");
 const button2 = document.querySelector("#button2");
 const button3 = document.querySelector("#button3");
 
-// Game Variables:
+// Visual Elements
+const goldText = document.querySelector("#goldText");
+const healthText = document.querySelector("#healthText");
+const monsterName = document.querySelector("#monsterName");
+const monsterStats = document.querySelector("monsterStats");
+const text = document.querySelector("#text");
+const xpText = document.querySelector("#xpText");
+/* --- END INTERFACE VARIABLES --- */
+
+/* --- GAME VARIABLES --- */
 let fighting;
 let monsterHealth;
+/* --- END GAME VARIABLES --- */
 
-// Player Stat Variables:
+/* --- PLAYER STAT VARIABLES --- */
 let currentWeaponIndex = 0;
 let gold = 50;
 let health = 100;
 let inventory = ["stick"];
 let xp = 0;
+/* --- END PLAYER STAT VARIABLES --- */
