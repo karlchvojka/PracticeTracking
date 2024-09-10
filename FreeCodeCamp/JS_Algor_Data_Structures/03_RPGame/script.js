f5f5ed700c0aa091161acbeccfec0268f4a337f8
karlchvojka <karl.chvojka@gmail.com>
2024-09-10 13:43:45 -0400

    IMP: FCC/JS/03_RPGame: Lesson 37

--- FreeCodeCamp/JS_Algor_Data_Structures/03_RPGame/script.js
+++ FreeCodeCamp/JS_Algor_Data_Structures/03_RPGame/script.js
@@ -69,5 +69,6 @@ function goStore() {
 
 // Interface Element Event Handlers
 button1.onclick = goStore;
-
+button2.onclick = goCave;
+button3.onclick = fightDragon;
 /* --- END EVENT HANDLER ASSIGNMENTS --- */
