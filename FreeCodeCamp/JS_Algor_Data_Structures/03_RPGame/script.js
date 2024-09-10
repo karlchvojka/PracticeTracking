5330f8aaf44bf4386d5734ec96c782949c570841
karlchvojka <karl.chvojka@gmail.com>
2024-09-10 14:09:45 -0400

    IMP: FCC/JS/03_RPGame: Lesson 43

--- FreeCodeCamp/JS_Algor_Data_Structures/03_RPGame/script.js
+++ FreeCodeCamp/JS_Algor_Data_Structures/03_RPGame/script.js
@@ -95,7 +95,18 @@ function goStore() {
  * Handles 'Go to town square' button press function.
  * @function goTown
  */
-function goTown() {}
+function goTown() {
+    //Reassign Element Text
+    button1.innerText = "Buy 10 health (10 gold)";
+    button2.innerText = "Buy weapon (30 gold)";
+    button3.innerText = "Go to town square";
+    text.innerText = "You enter the store.";
+
+    // Reassign Button Event Handlers
+    button1.onclick = buyHealth;
+    button2.onclick = buyWeapon;
+    button3.onclick = goTown;
+}
 
 /* --- END INTERFACE FUNCTION DECLARATIONS --- */
 
