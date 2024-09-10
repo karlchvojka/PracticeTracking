acbffdb882d101740c0c5e176e3bfe48cdf9be36
karlchvojka <karl.chvojka@gmail.com>
2024-09-10 16:14:56 -0400

    IMP: FCC/JS/03_RPGame: Lesson 94

--- FreeCodeCamp/JS_Algor_Data_Structures/03_RPGame/script.js
+++ FreeCodeCamp/JS_Algor_Data_Structures/03_RPGame/script.js
@@ -155,6 +155,8 @@ function buyWeapon() {
         // Update inventory
         inventory.push(newWeapon);
         text.innerText += " In your inventory you have: " + inventory;
+    } else {
+        text.innerText = "You do not have enough gold to buy a weapon.";
     }
 }
 
