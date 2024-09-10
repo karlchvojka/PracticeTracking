60763f6e7158da8cf8c453ea768298f9f367bb4a
karlchvojka <karl.chvojka@gmail.com>
2024-09-10 13:52:23 -0400

    IMP: FCC/JS/03_RPGame: Lesson 39

--- FreeCodeCamp/JS_Algor_Data_Structures/03_RPGame/script.js
+++ FreeCodeCamp/JS_Algor_Data_Structures/03_RPGame/script.js
@@ -62,6 +62,8 @@ function goCave() {
  */
 function goStore() {
     button1.innerText = "Buy 10 health (10 gold)";
+    button2.innerText = "Buy weapon (30 gold)";
+    button3.innerText = "Go to town square";
 }
 /* --- END INTERFACE FUNCTION DECLARATIONS --- */
 
