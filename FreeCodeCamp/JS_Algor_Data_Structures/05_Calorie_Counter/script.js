1036c66827c856953459daf5477ca99acb6d3be2
karlchvojka <karl.chvojka@gmail.com>
2024-09-11 18:04:56 -0400

    IMP: FCC/JS/05_Calorie_Counter: Lesson 70

--- FreeCodeCamp/JS_Algor_Data_Structures/05_Calorie_Counter/script.js
+++ FreeCodeCamp/JS_Algor_Data_Structures/05_Calorie_Counter/script.js
@@ -114,7 +114,9 @@ function getCaloriesFromInputs(list) {
 function calculateCalories(e) {
     e.preventDefault();
     isError = false;
+
     const breakfastNumberInputs = document.querySelectorAll("#breakfast input[type=number]");
+    const lunchNumberInputs = document.querySelectorAll("#lunch input[type=number]");
 }
 
 /* --- END GLOBAL FUNCTIONS ---*/
