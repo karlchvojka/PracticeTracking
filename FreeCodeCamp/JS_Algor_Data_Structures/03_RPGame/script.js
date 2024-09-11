42351188ee8f036f450204d620c6923728d990e4
karlchvojka <karl.chvojka@gmail.com>
2024-09-10 21:10:54 -0400

    IMP: FCC/JS/03_RPGame: Lesson 172

--- FreeCodeCamp/JS_Algor_Data_Structures/03_RPGame/script.js
+++ FreeCodeCamp/JS_Algor_Data_Structures/03_RPGame/script.js
@@ -565,5 +565,8 @@ function pick(guess) {
         text.innerText += "Wrong! You loose 10 health!";
         health -= 10;
         healthText.innerText = health;
+        if (health <= 0) {
+            lose();
+        }
     }
 }
