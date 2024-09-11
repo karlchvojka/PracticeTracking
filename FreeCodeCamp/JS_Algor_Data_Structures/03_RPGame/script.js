10ed5a45557b700df87cb0125f625ac2c9d130cc
karlchvojka <karl.chvojka@gmail.com>
2024-09-10 20:57:59 -0400

    IMP: FCC/JS/03_RPGame: Lesson 164

--- FreeCodeCamp/JS_Algor_Data_Structures/03_RPGame/script.js
+++ FreeCodeCamp/JS_Algor_Data_Structures/03_RPGame/script.js
@@ -550,5 +550,7 @@ function pickEight() {
  */
 function pick(guess) {
     const numbers = [];
-    while (numbers.length < 10) {}
+    while (numbers.length < 10) {
+        numbers.push(Math.floor(Math.random() * 11));
+    }
 }
