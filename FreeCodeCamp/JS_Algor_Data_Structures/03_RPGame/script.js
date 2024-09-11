cb7031fd0495e327f5adae85a8514f6392b70e6c
karlchvojka <karl.chvojka@gmail.com>
2024-09-10 20:54:28 -0400

    IMP: FCC/JS/03_RPGame: Lesson 161

--- FreeCodeCamp/JS_Algor_Data_Structures/03_RPGame/script.js
+++ FreeCodeCamp/JS_Algor_Data_Structures/03_RPGame/script.js
@@ -105,6 +105,20 @@ const locations = [
             "REPLAY?"
         ],
         text: "You defeat the dragon! YOU WIN THE GAME! &#x1F389;"
+    },
+    {
+        name: "easter egg",
+        "button func": [
+            pickTwo,
+            pickEight,
+            goTown
+        ],
+        "button text": [
+            "2",
+            "8",
+            "Go to town square?"
+        ],
+        text: "You find a secret game. Pick a number above. Ten numbers will be randomly chosen between 0 and 10. If the number you choose matches one of the random numbers, you win!"
     }
 ];
 const monsters = [
