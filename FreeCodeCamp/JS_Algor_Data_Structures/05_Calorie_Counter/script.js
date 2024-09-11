f9af2b257a955db08796092b40babfa19881faab
karlchvojka <karl.chvojka@gmail.com>
2024-09-11 16:39:22 -0400

    IMP: FCC/JS/05_Calorie_Counter: Lesson 48

--- FreeCodeCamp/JS_Algor_Data_Structures/05_Calorie_Counter/script.js
+++ FreeCodeCamp/JS_Algor_Data_Structures/05_Calorie_Counter/script.js
@@ -62,8 +62,9 @@ function isInvalidInput(str) {
 function addEntry() {
     const targetInputContainer = document.querySelector(`#${entryDropdown.value} .input-container`);
     const entryNumber = targetInputContainer.querySelectorAll('input[type="text"]').length;
-    const HTMLString = `<label for="${entryDropdown.value}-${entryNumber}-name">Entry ${entryNumber} Name</label>`;
-
+    const HTMLString = `
+    <label for="${entryDropdown.value}-${entryNumber}-name">Entry ${entryNumber} Name</label>
+    <input id="${entryDropdown.value}-${entryNumber}-name" placeholder="Name" type="text" />`;
 }
 
 /* --- END GLOBAL FUNCTIONS ---*/
