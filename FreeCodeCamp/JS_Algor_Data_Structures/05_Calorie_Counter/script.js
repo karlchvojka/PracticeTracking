469e9f1506ebf1d23550b89fe102f6f5a6600e2a
karlchvojka <karl.chvojka@gmail.com>
2024-09-11 14:49:17 -0400

    IMP: FCC/JS/05_Calorie_Counter: Lesson 15

--- FreeCodeCamp/JS_Algor_Data_Structures/05_Calorie_Counter/script.js
+++ FreeCodeCamp/JS_Algor_Data_Structures/05_Calorie_Counter/script.js
@@ -1,2 +1,4 @@
 /* --- Interface Variables --- */
+const budgetNumberInput = document.getElementById('budget');
 const calorieCounter = document.getElementById('calorie-counter');
+const entryDropdown = document.getElementById('entry-dropdown');
