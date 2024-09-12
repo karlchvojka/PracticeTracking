c37c3a1892d5939e161166811f7f85fbb4933a6c
karlchvojka <karl.chvojka@gmail.com>
2024-09-11 20:00:43 -0400

    IMP: FCC/JS/05_Calorie_Counter: Lesson 85

--- FreeCodeCamp/JS_Algor_Data_Structures/05_Calorie_Counter/script.js
+++ FreeCodeCamp/JS_Algor_Data_Structures/05_Calorie_Counter/script.js
@@ -112,6 +112,7 @@ function calculateCalories(e) {
     output.innerHTML = `
         <span class="${surplusOrDeficit.toLowerCase()}">${Math.abs(remainingCalories)} Calorie ${surplusOrDeficit}</span>
         <hr />
+        <p>${budgetCalories} Calories Budgeted</p>
     `;
 }
 
