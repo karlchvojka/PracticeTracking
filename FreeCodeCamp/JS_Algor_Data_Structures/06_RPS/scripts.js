eea560426538f51deceeb169a46bbef0fa9c63a1
karlchvojka <karl.chvojka@gmail.com>
2024-09-21 09:12:33 -0400

    INIT: Setting up basics for project
    Base file setup and initiation

---
+++ FreeCodeCamp/JS_Algor_Data_Structures/06_RPS/scripts.js
@@ -0,0 +1,12 @@
+/**
+ *  Rock Paper Scissors game.
+ *  
+ *  Rock Paper Scissors game project from the 'JS Algorithms and Data Structures' section of the FCC Curriculum
+ */
+
+/* -- INTERFACE VARIABLES -- */
+
+/* --- END INTERFACE VARIABLES ---*/
+
+/* --- GLOBAL FUNCTIONS --- */
+/* --- END GLOBAL FUNCTIONS --- */
