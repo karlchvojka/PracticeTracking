447b1b18a6a5c18f8226792dff3551be77faf323
karlchvojka <karl.chvojka@gmail.com>
2024-09-23 01:03:51 -0400

    IMP: FCC/JS/07_MusicPlayer: Lesson 65

--- FreeCodeCamp/JS_Algor_Data_Structures/07_MusicPlayer/scripts.js
+++ FreeCodeCamp/JS_Algor_Data_Structures/07_MusicPlayer/scripts.js
@@ -123,6 +123,10 @@ const highlightCurrentSong = () => {
     playlistSongElements.forEach((songEl) => {
         songEl.removeAttribute("aria-current");
     });
+
+    if (songToHighlight) {
+        songToHighlight.setAttribute("aria-current", "true");
+    }
 };
 
 
