b2828cddceb2158eac00a85c880c8690d275972e
karlchvojka <karl.chvojka@gmail.com>
2024-09-22 21:45:44 -0400

    IMP: FCC/JS/07_MusicPlayer: Lesson 6

--- FreeCodeCamp/JS_Algor_Data_Structures/07_MusicPlayer/scripts.js
+++ FreeCodeCamp/JS_Algor_Data_Structures/07_MusicPlayer/scripts.js
@@ -28,6 +28,13 @@ const allSongs = [
         artist: "Quincy Larson",
         duration: "4:15",
         src: "https://cdn.freecodecamp.org/curriculum/js-music-player/can't-stay-down.mp3"
+    },
+    {
+        id: 2,
+        title: "Still Learning",
+        artist: "Quincy Larson",
+        duration: "3:51",
+        src: "https://cdn.freecodecamp.org/curriculum/js-music-player/still-learning.mp3"
     }
 ];
 
