806b22f1ab14d0673832d7faa20d1c19dbcd3c34
karlchvojka <karl.chvojka@gmail.com>
2024-09-22 23:46:39 -0400

    IMP: FCC/JS/07_MusicPlayer: Lesson 37

--- FreeCodeCamp/JS_Algor_Data_Structures/07_MusicPlayer/scripts.js
+++ FreeCodeCamp/JS_Algor_Data_Structures/07_MusicPlayer/scripts.js
@@ -109,6 +109,8 @@ const audio = new Audio();
  */
 const playSong = (id) => {
     const song = userData?.songs.find((song) => song.id === id);
+    audio.src = song.src;
+    audio.title = song.title;
 };
 
 /**
