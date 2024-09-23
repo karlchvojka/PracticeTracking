637ca0f7069eef46dda9f3fe5e7f2432628af6b1
karlchvojka <karl.chvojka@gmail.com>
2024-09-23 16:25:17 -0400

    IMP: Test to check base submit functionality

--- FreeCodeCamp/JS_Algor_Data_Structures/08_Palindrome_Checker/scripts.js
+++ FreeCodeCamp/JS_Algor_Data_Structures/08_Palindrome_Checker/scripts.js
@@ -5,13 +5,27 @@
  */
 
 /* --- INTERFACE VARIABLES --- */
+const paliCheckForm = document.getElementById("palindrome-check-form");
+const stringInput = document.getElementById("text-input");
+const submitButton = document.getElementById("check-btn");
 /* --- END INTERFACE VARIABLES --- */
 
-/* --- APP DATA --- */
-/* --- END APP DATA --- */
-
 /* --- APP LOGIC --- */
+
+/**
+ * Check String
+ *
+ * Checks the submitted string to see if its a palintrome.
+ * @function checkString
+ * @param {event} event Form Submit Event.
+ */
+const checkString = (event) => {
+    console.log("Test");
+    event.preventDefault();
+}
+
 /* --- END APP LOGIC  --- */
 
 /* --- EVENT LISTENERS --- */
+paliCheckForm.addEventListener("submit", checkString);
 /* --- END EVENT LISTENERS --- */
