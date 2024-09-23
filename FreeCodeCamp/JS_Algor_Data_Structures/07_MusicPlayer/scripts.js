804fcfd751bd531c8e2c944779e2b871e542d15e
karlchvojka <karl.chvojka@gmail.com>
2024-09-22 21:32:09 -0400

    IMP: FCC/JS/07_MusicPlayer: Lesson 1

---
+++ FreeCodeCamp/JS_Algor_Data_Structures/07_MusicPlayer/scripts.js
@@ -0,0 +1,11 @@
+/**
+ * Music Player Application.
+ *
+ * Music Player Application from the 'JS Algorithms and Data Structures' section of the FCC Curriculum
+ */
+
+/* --- INTERFACE VARIABLES --- */
+const playButton = document.getElementById("play");
+const playlistSongs = document.getElementById("playlistSongs");
+const pauseButton = document.getElementById("pause");
+/* --- END INTERFACE VARIABLES --- */
