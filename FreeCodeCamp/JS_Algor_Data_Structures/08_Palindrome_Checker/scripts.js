dc3fa126c82ba3c6e4a43a7ca500700aa355e983
karlchvojka <karl.chvojka@gmail.com>
2024-09-23 02:29:37 -0400

    INIT: FCC/JS/08_Palindrome_check: Setup
    Initial file and folder setup

---
+++ FreeCodeCamp/JS_Algor_Data_Structures/08_Palindrome_Checker/scripts.js
@@ -0,0 +1,17 @@
+/**
+ * Palindrome Checker Application.
+ *
+ * Application that checks if entered string of text is a palinrome or not.
+ */
+
+/* --- INTERFACE VARIABLES --- */
+/* --- END INTERFACE VARIABLES --- */
+
+/* --- APP DATA --- */
+/* --- END APP DATA --- */
+
+/* --- APP LOGIC --- */
+/* --- END APP LOGIC  --- */
+
+/* --- EVENT LISTENERS --- */
+/* --- END EVENT LISTENERS --- */
