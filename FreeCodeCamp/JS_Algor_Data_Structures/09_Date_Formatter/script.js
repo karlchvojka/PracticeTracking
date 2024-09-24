bef15fa47498e00eb8625e972025dd815afe6354
karlchvojka <karl.chvojka@gmail.com>
2024-09-23 21:59:06 -0400

    IMP: FCC/JS/09_Date_formatter: Lesson 25

--- FreeCodeCamp/JS_Algor_Data_Structures/09_Date_Formatter/script.js
+++ FreeCodeCamp/JS_Algor_Data_Structures/09_Date_Formatter/script.js
@@ -24,7 +24,7 @@ currentDateParagraph.textContent = formattedDate;
 dateOptionsSelectElement.addEventListener("change", () => {
     switch (dateOptionsSelectElement.value) {
         case 'yyyy-mm-dd':
-            currentDateParagraph.textContent = formattedDate.split("-").reverse();
+            currentDateParagraph.textContent = formattedDate.split("-").reverse().join("-");
             break;
     }
 });
