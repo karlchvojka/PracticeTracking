6a25bfd14eaf6ef8e9035bf2c96f09dc2dff8130
karlchvojka <karl.chvojka@gmail.com>
2024-09-23 21:29:28 -0400

    IMP: FCC/JS/09_Date_formatter: Lesson 5

--- FreeCodeCamp/JS_Algor_Data_Structures/09_Date_Formatter/script.js
+++ FreeCodeCamp/JS_Algor_Data_Structures/09_Date_Formatter/script.js
@@ -9,4 +9,5 @@ const currentDateParagraph = document.getElementById("current-date");
 const date = new Date();
 const dateOptionsSelectElement = document.getElementById("date-options");
 const day = date.getDate();
+const month = date.getMonth();
 /* --- END INTERFACE VARIABLES --- */
