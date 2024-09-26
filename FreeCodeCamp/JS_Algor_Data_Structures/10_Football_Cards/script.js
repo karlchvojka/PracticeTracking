eabe7699e1488c53ea3daf610273dac1fac1e008
karlchvojka <karl.chvojka@gmail.com>
2024-09-26 14:31:42 -0400

    INIT: File and folder setup

---
+++ FreeCodeCamp/JS_Algor_Data_Structures/10_Football_Cards/script.js
@@ -0,0 +1,15 @@
+/**
+ * Football Team Cards
+ *
+ * Football Team Cards Project from FreeCodeCamp Curriculum
+ */
+
+/* --- INTERFACE VARIABLES --- */
+/* --- END INTERFACE VARIABLES --- */
+
+/* --- APP LOGIC --- */
+/* --- END APP LOGIC --- */
+
+/* --- EVENT LISTENERS --- */
+/* --- END EVENT LISTENERS --- */
+
