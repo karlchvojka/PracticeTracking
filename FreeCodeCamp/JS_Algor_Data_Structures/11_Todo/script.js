b30ddbfb0308e961bc3a49b76ba3a6e7ae39f45c
karlchvojka <karl.chvojka@gmail.com>
2024-09-26 18:17:12 -0400

    IMP: FCC/JS/11_Todo: Lesson 1

---
+++ FreeCodeCamp/JS_Algor_Data_Structures/11_Todo/script.js
@@ -0,0 +1,20 @@
+/**
+ * Todo App
+ *
+ * Todo App: freeCodeCamp JS and Data Structures curriculum
+ */ 
+
+/* --- INTERFACE VARIABLES --- */
+const confirmCloseDialog = document.getElementById("confirm-close-dialog");
+const openTaskFormBtn = document.getElementById("open-task-form-btn");
+const taskForm = document.getElementById("task-form");
+/* --- END INTERFACE VARIABLES --- */
+
+/* --- APP DATA --- */
+/* --- END APP DATA --- */
+
+/* --- APP LOGIC --- */
+/* --- END APP LOGIC --- */
+
+/* --- EVENT LISTENERS --- */
+/* --- END EVENT LISTENERS --- */
