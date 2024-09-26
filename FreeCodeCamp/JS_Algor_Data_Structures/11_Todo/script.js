09d526af5ea05c4c5ec3d3bb012c2ac13ac95325
karlchvojka <karl.chvojka@gmail.com>
2024-09-26 18:25:00 -0400

    IMP: FCC/JS/11_Todo: Lesson 3

--- FreeCodeCamp/JS_Algor_Data_Structures/11_Todo/script.js
+++ FreeCodeCamp/JS_Algor_Data_Structures/11_Todo/script.js
@@ -9,8 +9,11 @@ const addOrUpdateTaskBtn = document.getElementById("add-or-update-task-btn");
 const cancelBtn = document.getElementById("cancel-btn");
 const closeTaskFormBtn = document.getElementById("close-task-form-btn");
 const confirmCloseDialog = document.getElementById("confirm-close-dialog");
+const discardBtn = document.getElementById("discard-btn");
 const openTaskFormBtn = document.getElementById("open-task-form-btn");
+const tasksContainer = document.getElementById("tasks-container");
 const taskForm = document.getElementById("task-form");
+const titleInput = document.getElementById("title-input");
 /* --- END INTERFACE VARIABLES --- */
 
 /* --- APP DATA --- */
