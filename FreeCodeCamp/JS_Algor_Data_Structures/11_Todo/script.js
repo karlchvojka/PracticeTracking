0e49b678d7674593772d77614cebfe7887514b35
karlchvojka <karl.chvojka@gmail.com>
2024-09-30 19:17:26 -0400

    IMP: FCC/JS/11_TODO: Lesson 22

--- FreeCodeCamp/JS_Algor_Data_Structures/11_Todo/script.js
+++ FreeCodeCamp/JS_Algor_Data_Structures/11_Todo/script.js
@@ -60,5 +60,7 @@ taskForm.addEventListener("submit", (e) => {
     if (dataArrIndex === -1) {
         taskData.unshift(taskObj);
     }
+
+    taskData.forEach(({id, title, date, description}) => {});
 });
 /* --- END EVENT LISTENERS --- */
