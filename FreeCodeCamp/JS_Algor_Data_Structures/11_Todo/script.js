7972eb5c125a7a245fc3e4333571cfcd6dcabe6e
karlchvojka <karl.chvojka@gmail.com>
2024-09-30 20:46:50 -0400

    IMP: FCC/JS/11_TODO: Lesson 53

--- FreeCodeCamp/JS_Algor_Data_Structures/11_Todo/script.js
+++ FreeCodeCamp/JS_Algor_Data_Structures/11_Todo/script.js
@@ -140,7 +140,7 @@ closeTaskFormBtn.addEventListener("click", () => {
     const formInputValuesUpdated = titleInput.value !== currentTask.title || dateInput.value !== currentTask.date || descriptionInput.value !== currentTask.description;
 
     // If there are values, show confirm close dialog. Else reset form:
-    if (formInputsContainValues) {
+    if (formInputsContainValues && formInputValuesUpdated) {
         confirmCloseDialog.showModal();
     } else {
         reset();
