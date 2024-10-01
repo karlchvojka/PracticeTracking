4639a0d42741963b355f339185d0a5ec9ff39933
karlchvojka <karl.chvojka@gmail.com>
2024-09-30 20:06:04 -0400

    IMP: FCC/JS/11_TODO: Lesson 37

--- FreeCodeCamp/JS_Algor_Data_Structures/11_Todo/script.js
+++ FreeCodeCamp/JS_Algor_Data_Structures/11_Todo/script.js
@@ -63,6 +63,26 @@ const reset = () => {
     // Reset current task obj
     currentTask = {};
 };
+
+/**
+ * Update Task Container
+ *
+ * Adds new task to Task Container
+ * @function updateTaskContainer
+ */
+const updateTaskContainer = () => {
+    taskData.forEach(({ id, title, date, description }) => {
+        tasksContainer.innerHTML += `
+            <div class="task" id="${id}">
+                <p><strong>Title:</strong> ${title}</p>
+                <p><strong>Date:</strong> ${date}</p>
+                <p><strong>Description:</strong> ${description}</p>
+                <button type="button" class="btn">Edit</button>
+                <button type="button" class="btn">Delete</button>
+            </div>
+        `
+    });
+};
 /* --- END APP LOGIC --- */
 
 /* --- EVENT LISTENERS --- */
