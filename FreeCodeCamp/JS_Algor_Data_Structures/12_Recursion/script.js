058b4ef1be908770e44a6e8b3c5517588af87c9d
karlchvojka <karl.chvojka@gmail.com>
2024-10-17 19:05:48 -0400

    IMP: FCC/JS/12_Recursion: Lesson 47

--- FreeCodeCamp/JS_Algor_Data_Structures/12_Recursion/script.js
+++ FreeCodeCamp/JS_Algor_Data_Structures/12_Recursion/script.js
@@ -5,6 +5,7 @@
  */
 
 /* --- INTERFACE VARIABLES --- */
+const callStack = [];
 const convertBtn = document.getElementById("convert-btn");
 const numberInput = document.getElementById("number-input");
 const result = document.getElementById("result");
