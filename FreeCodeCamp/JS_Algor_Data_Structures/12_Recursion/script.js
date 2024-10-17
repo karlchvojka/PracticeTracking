4f3ea03a9bf48e0893b585755fbec9df5b426a6d
karlchvojka <karl.chvojka@gmail.com>
2024-10-17 19:30:55 -0400

    IMP: FCC/JS/12_Recursion: Lesson 58

--- FreeCodeCamp/JS_Algor_Data_Structures/12_Recursion/script.js
+++ FreeCodeCamp/JS_Algor_Data_Structures/12_Recursion/script.js
@@ -19,6 +19,9 @@ const result = document.getElementById("result");
  */
 const countdown = (number) => {
     console.log(number);
+    if (number === 0) {
+        return;
+    }
 }
 /**
  * Decimal To Binary
