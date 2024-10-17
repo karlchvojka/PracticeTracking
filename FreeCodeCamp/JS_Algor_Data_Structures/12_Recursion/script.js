5668f2c9c44d5d1a33a9dbf7eb4608fdd34ed30d
karlchvojka <karl.chvojka@gmail.com>
2024-10-17 18:48:42 -0400

    IMP: FCC/JS/12_Recursion: Lesson 40

--- FreeCodeCamp/JS_Algor_Data_Structures/12_Recursion/script.js
+++ FreeCodeCamp/JS_Algor_Data_Structures/12_Recursion/script.js
@@ -19,6 +19,7 @@ const result = document.getElementById("result");
 const decimalToBinary = (input) => {
     let binary = "";
     while (input > 0) {
+        binary = input % 2;
         input = Math.floor(input / 2);
     }
     result.innerText = binary;
