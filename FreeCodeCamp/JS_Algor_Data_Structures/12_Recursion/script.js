cf730dbd5aa305625fa922b1663d6e8c6afb523f
karlchvojka <karl.chvojka@gmail.com>
2024-10-17 15:34:00 -0400

    IMP: FCC/JS/12_Recursion: Lesson 13

--- FreeCodeCamp/JS_Algor_Data_Structures/12_Recursion/script.js
+++ FreeCodeCamp/JS_Algor_Data_Structures/12_Recursion/script.js
@@ -20,6 +20,8 @@ const checkUserInput = () => {
         !numberInput.value ||
         isNaN(parseInt(numberInput.value)) ||
         parseInt(numberInput.value) < 0) {
+
+        alert("Please provide a decimal number greater than or equal to 0");
     };
     console.log(numberInput.value);
 }
