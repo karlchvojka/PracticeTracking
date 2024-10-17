70fadef7e083e1dcd43f8fb1121bd3ca838f5f10
karlchvojka <karl.chvojka@gmail.com>
2024-10-17 13:23:10 -0400

    IMP: FCC/JS/12_Recursion: Lesson 2

--- FreeCodeCamp/JS_Algor_Data_Structures/12_Recursion/script.js
+++ FreeCodeCamp/JS_Algor_Data_Structures/12_Recursion/script.js
@@ -1,3 +1,21 @@
+/**
+ * Decimal to Binary Converter
+ *
+ * Recursion Project: Lesson 11: JS Algorithms and Data Structures: FCC
+ */
+
+/* --- INTERFACE VARIABLES --- */
 const convertBtn = document.getElementById("convert-btn");
 const numberInput = document.getElementById("number-input");
 const result = document.getElementById("result");
+/* --- END INTERFACE VARIABLES --- */
+
+/* --- APP LOGIC --- */
+/**
+ * Check User Input.
+ * @function
+ */
+const checkUserInput = () => {}
+
+/* --- END APP LOGIC --- */
+
