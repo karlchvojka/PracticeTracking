e973f0e03a42f3dde93c62342896726688092604
karlchvojka <karl.chvojka@gmail.com>
2024-10-17 17:40:28 -0400

    IMP: FCC/JS/12_Recursion: Lesson 26

--- FreeCodeCamp/JS_Algor_Data_Structures/12_Recursion/script.js
+++ FreeCodeCamp/JS_Algor_Data_Structures/12_Recursion/script.js
@@ -22,7 +22,7 @@ const decimalToBinary = (input) => {
     const remainders = [];
 
     while (input > 0) {
-        const quotient = input / 2;
+        const quotient = Math.floor(input / 2);
         input = 0;
     }
 }
