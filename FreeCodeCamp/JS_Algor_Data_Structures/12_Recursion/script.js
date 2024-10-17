b728a8715eda875bdaa4f567c73895b1c1ea0742
karlchvojka <karl.chvojka@gmail.com>
2024-10-17 16:58:53 -0400

    IMP: FCC/JS/12_Recursion: Lesson 17

--- FreeCodeCamp/JS_Algor_Data_Structures/12_Recursion/script.js
+++ FreeCodeCamp/JS_Algor_Data_Structures/12_Recursion/script.js
@@ -34,6 +34,7 @@ const checkUserInput = () => {
         return;
     };
     decimalToBinary(parseInt(numberInput.value));
+    numberInput.value = "";
 }
 
 convertBtn.addEventListener("click", checkUserInput);
