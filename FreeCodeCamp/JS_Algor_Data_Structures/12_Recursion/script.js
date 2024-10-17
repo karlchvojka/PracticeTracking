540b4204c388dca366b5e2a3bc226402b5836463
karlchvojka <karl.chvojka@gmail.com>
2024-10-17 18:24:30 -0400

    IMP: FCC/JS/12_Recursion: Lesson 32

--- FreeCodeCamp/JS_Algor_Data_Structures/12_Recursion/script.js
+++ FreeCodeCamp/JS_Algor_Data_Structures/12_Recursion/script.js
@@ -30,6 +30,8 @@ const decimalToBinary = (input) => {
         input = quotient;
     }
     console.log("Inputs: ", inputs);
+    console.log("Quotients: ", quotients);
+    console.log("Remainders: ", remainders);
 }
 
 /**
