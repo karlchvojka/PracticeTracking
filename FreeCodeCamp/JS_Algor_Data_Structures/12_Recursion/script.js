af1f6fd1bfaf34357ae8c1abf32faf3ff8259797
karlchvojka <karl.chvojka@gmail.com>
2024-10-18 13:05:08 -0400

    IMP: FCC/JS/12_Recursion: Lesson 75

--- FreeCodeCamp/JS_Algor_Data_Structures/12_Recursion/script.js
+++ FreeCodeCamp/JS_Algor_Data_Structures/12_Recursion/script.js
@@ -37,6 +37,9 @@ const checkUserInput = () => {
         alert("Please provide a decimal number greater than or equal to 0"      );
         return;
     };
+    
+    if (parseInt(numberInput.value) === 5) {}
+
     result.textContent = decimalToBinary(parseInt(numberInput.value));
     numberInput.value = "";
 }
