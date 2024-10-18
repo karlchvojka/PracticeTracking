29c2c39011e12af21b7ef76c4319cf576595016f
karlchvojka <karl.chvojka@gmail.com>
2024-10-18 14:19:50 -0400

    IMP: FCC/JS/12_Recursion: Lesson 106

--- FreeCodeCamp/JS_Algor_Data_Structures/12_Recursion/script.js
+++ FreeCodeCamp/JS_Algor_Data_Structures/12_Recursion/script.js
@@ -90,7 +90,7 @@ const showAnimation = () => {
             `;
         }, obj.addElDelay);
         setTimeout(() => {document.getElementById(obj.inputVal).textContent = obj.msg;}, obj.showMsgDelay);
-        setTimeout(() => {}, obj.removeElDelay);
+        setTimeout(() => {document.getElementById(obj.inputVal).remove()}, obj.removeElDelay);
     });
 }
 
