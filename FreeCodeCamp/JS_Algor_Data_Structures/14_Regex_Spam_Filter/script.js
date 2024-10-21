c8f85ded6a76d120e0ddf6f8efcc8fed30276df4
karlchvojka <karl.chvojka@gmail.com>
2024-10-21 14:49:27 -0400

    IMP: FCC/JS/14_Regex_Spam_Filter: Lesson 1
    Add interface elements to variables

---
+++ FreeCodeCamp/JS_Algor_Data_Structures/14_Regex_Spam_Filter/script.js
@@ -0,0 +1,20 @@
+/**
+ * Spam Filter
+ *
+ * Regex Project: Learn Regex by building a spam filter.
+ */
+
+/* --- INTERFACE VARIABLES --- */
+const checkMessageButton = document.getElementById('check-message-btn')
+const messageInput = document.getElementById('message-input');
+const result = document.getElementById('result');
+/* --- END INTERFACE VARIABLES --- */
+
+/* --- APP DATA --- */
+/* --- END APP DATA --- */
+
+/* --- EVENT LISTENERS --- */
+/* --- END EVENT LISTENERS --- */
+
+/* --- APP LOGIC --- */
+/* --- END APP LOGIC --- */
