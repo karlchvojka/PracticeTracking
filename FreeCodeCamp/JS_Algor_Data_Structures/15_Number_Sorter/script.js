e9e4a622a73fcc4f68eb49a4747237e6308a1134
karlchvojka <karl.chvojka@gmail.com>
2024-10-24 20:23:35 -0400

    IMP: FCC/JS/15_Number_Sorter: Lesson 24

--- FreeCodeCamp/JS_Algor_Data_Structures/15_Number_Sorter/script.js
+++ FreeCodeCamp/JS_Algor_Data_Structures/15_Number_Sorter/script.js
@@ -20,7 +20,6 @@ const sortButton = document.getElementById("sort");
 const bubbleSort = (array) => {
     for (let i=0; i < array.length; i++) {
         for (let j = 0; j < array.length - 1; j++) {
-            console.log(array, array[j], array[j+1]);
             if (array[j] > array[j+1]) {
                 const temp = array[j];
                 array[j] = array[j + 1];
