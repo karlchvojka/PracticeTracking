5e99bd0ac0b69ca1e1105c3385d4a4d817234603
karlchvojka <karl.chvojka@gmail.com>
2024-10-24 21:12:02 -0400

    IMP: FCC/JS/15_Number_Sorter: Lesson 39

--- FreeCodeCamp/JS_Algor_Data_Structures/15_Number_Sorter/script.js
+++ FreeCodeCamp/JS_Algor_Data_Structures/15_Number_Sorter/script.js
@@ -39,10 +39,11 @@ const insertionSort = (array) => {
     for (let i = 1; i < array.length; i++) {
         const currValue = array[i];
         let j = i - 1;
-    while (j >= 0 && array[j] > currValue) {
-        array[j + 1] = array[j];
-        j--;
-    }
+        while (j >= 0 && array[j] > currValue) {
+            array[j + 1] = array[j];
+            j--;
+        }
+        array[j + 1] = currValue;
     }
 }
 /**
