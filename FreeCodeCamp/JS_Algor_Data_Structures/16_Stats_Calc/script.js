bf1ca3ada34738e386e852a4c71e4a60c22360d3
karlchvojka <karl.chvojka@gmail.com>
2024-10-28 15:40:34 -0400

    IMP: FCC/JS/16_Stats_Calc: Lesson 1

---
+++ FreeCodeCamp/JS_Algor_Data_Structures/16_Stats_Calc/script.js
@@ -0,0 +1,20 @@
+/**
+ * Statistics Calculator
+ *
+ * Will take a set of numbers and returns the mean, median, mode, standard deviation, and variance.
+ */
+
+/* --- INTERFACE VARIABLES --- */
+/* --- END INTERFACE VARIABLES --- */
+
+/* --- APP DATA --- */
+/* --- END APP DATA --- */
+
+/* --- APP LOGIC --- */
+/**
+ * Handles submitted numbers
+ * @function calculate
+ */
+const calculate = () => {
+};
+/* --- END APP LOGIC --- */
