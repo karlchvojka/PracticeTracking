d70d028ccd43f68144ec19deb37c05596efe9fe0
karlchvojka <karl.chvojka@gmail.com>
2024-10-28 21:04:05 -0400

    IMP: FCC/JS/16_Stats_Calc: Lesson 25

--- FreeCodeCamp/JS_Algor_Data_Structures/16_Stats_Calc/script.js
+++ FreeCodeCamp/JS_Algor_Data_Structures/16_Stats_Calc/script.js
@@ -19,18 +19,6 @@
  */
 const getMean = (array) => array.reduce((acc, el) => acc + el, 0) / array.length;
 
-const testArr1 = [1, 2, 3, 4, 5];
-const testArr2 = [1, 2, 3, 4, 5, 6];
-
-const isEven = testArr2.length % 2 === 0;
-console.log(isEven);
-
-const oddListMedian = testArr1[Math.floor(testArr1.length / 2)];
-console.log(oddListMedian);
-
-const evenListMedian = getMean(testArr2);
-console.log(evenListMedian);
-
 /**
  * Get Median of submitted array
  * @function getMedian
