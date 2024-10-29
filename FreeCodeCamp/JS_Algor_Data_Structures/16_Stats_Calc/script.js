26c44a2a6c1610cd3644eff56150afdcc4e6857c
karlchvojka <karl.chvojka@gmail.com>
2024-10-28 22:56:04 -0400

    IMP: FCC/JS/16_Stats_Calc: Lesson 52

--- FreeCodeCamp/JS_Algor_Data_Structures/16_Stats_Calc/script.js
+++ FreeCodeCamp/JS_Algor_Data_Structures/16_Stats_Calc/script.js
@@ -107,8 +107,9 @@ const getVariance = (array) => {
         const difference = el - mean;
         const squared = difference ** 2;
         return acc + squared;
-    }, 0);
-}
+    }, 0) / array.length;
+    return variance;
+};
 
 /**
  * Handles submitted numbers
