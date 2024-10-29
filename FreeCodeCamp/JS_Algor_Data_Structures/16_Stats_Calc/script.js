7f3c4c191edd1e7fcdcfeb21813743aa2cfa7d17
karlchvojka <karl.chvojka@gmail.com>
2024-10-28 23:02:19 -0400

    IMP: FCC/JS/16_Stats_Calc: Lesson 55

--- FreeCodeCamp/JS_Algor_Data_Structures/16_Stats_Calc/script.js
+++ FreeCodeCamp/JS_Algor_Data_Structures/16_Stats_Calc/script.js
@@ -118,6 +118,7 @@ const getVariance = (array) => {
  */
 const getStandardDeviation = (array) => {
     const variance = getVariance(array);
+    const standardDeviation = Math.pow(variance, 1/2);
 }
 
 /**
