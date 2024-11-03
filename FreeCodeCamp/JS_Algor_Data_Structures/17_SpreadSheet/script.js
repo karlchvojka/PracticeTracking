ba0a4794d9d0ca5f3db932abcd66cba898abd55f
karlchvojka <karl.chvojka@gmail.com>
2024-11-03 16:34:48 -0500

    IMP: FCC/JS/17_SpreadSheet: Lesson 8

--- FreeCodeCamp/JS_Algor_Data_Structures/17_SpreadSheet/script.js
+++ FreeCodeCamp/JS_Algor_Data_Structures/17_SpreadSheet/script.js
@@ -33,7 +33,7 @@ window.onload = () => {
      * @param {number} end End of range
      * @returns {string} Returned range
      */
-    const range = (start, end) => Array(end - start + 1);
+    const range = (start, end) => Array(end - start + 1).fill(start);
 }
 /* --- END INTERFACE VARIABILES --- */
 
