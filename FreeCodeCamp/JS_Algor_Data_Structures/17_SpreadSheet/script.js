53065cf0035452799043b1ca16619b45983d6ed9
karlchvojka <karl.chvojka@gmail.com>
2024-11-03 16:13:22 -0500

    IMP: FCC/JS/17_SpreadSheet: Lesson 1

---
+++ FreeCodeCamp/JS_Algor_Data_Structures/17_SpreadSheet/script.js
@@ -0,0 +1,20 @@
+/**
+ * Functional Programming SpreadSheet
+ *
+ * Will build a SpreadSheet as an example of Functional Programming.
+ */
+
+/* --- INTERFACE VARIABLES --- */
+/**
+ * Windows Onload Event
+ */
+window.onload = () => {
+    const container = document.getElementById('container');
+}
+/* --- END INTERFACE VARIABILES --- */
+
+/* --- APP DATA --- */
+/* --- END APP DATA --- */
+
+/* --- APP LOGIC --- */
+/* --- END APP LOGIC --- */
