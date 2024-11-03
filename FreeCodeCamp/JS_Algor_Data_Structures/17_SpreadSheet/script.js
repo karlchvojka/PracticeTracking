99170c5f0be81c6840191b89a7a8f304c71c7d43
karlchvojka <karl.chvojka@gmail.com>
2024-11-03 18:31:49 -0500

    IMP: FCC/JS/17_SpreadSheet: Lesson 24

--- FreeCodeCamp/JS_Algor_Data_Structures/17_SpreadSheet/script.js
+++ FreeCodeCamp/JS_Algor_Data_Structures/17_SpreadSheet/script.js
@@ -29,6 +29,8 @@
      */
     const median = (nums) => {
         const sorted = nums.slice().sort((a, b) => a - b);
+        const length = sorted.length;
+        const middle = length / 2 - 1;
     };
 
     /**
