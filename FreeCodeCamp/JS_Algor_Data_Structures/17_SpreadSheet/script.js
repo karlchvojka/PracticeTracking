79d5fcc5640a4fda3337863ecc1b3ff51737ccbc
karlchvojka <karl.chvojka@gmail.com>
2024-11-04 10:30:27 -0500

    IMP: FCC/JS/17_SpreadSheet: Lesson 104

--- FreeCodeCamp/JS_Algor_Data_Structures/17_SpreadSheet/script.js
+++ FreeCodeCamp/JS_Algor_Data_Structures/17_SpreadSheet/script.js
@@ -106,6 +106,7 @@ const spreadsheetFunctions = {
     increment: nums => nums.map((num) => num + 1),
     lasttwo: nums => nums.slice(-2),
     median,
+    nodupes: nums => [...new Set(nums)],
     random: ([x, y]) => Math.floor(Math.random() * y + x),
     range: nums => range(...nums),
     someeven: nums => nums.some((num) => isEven(num)),
