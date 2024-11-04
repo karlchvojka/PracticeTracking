75b9df816deb0cbf333b2b88b57cd318d5478acb
karlchvojka <karl.chvojka@gmail.com>
2024-11-03 21:34:24 -0500

    IMP: FCC/JS/17_SpreadSheet: Lesson 85

--- FreeCodeCamp/JS_Algor_Data_Structures/17_SpreadSheet/script.js
+++ FreeCodeCamp/JS_Algor_Data_Structures/17_SpreadSheet/script.js
@@ -112,6 +112,7 @@ const applyFunction = (str) => {
     const str2 = infixEval(noHigh, infix);
     const functionCall = /([a-z0-9]*)\(([0-9., ]*)\)(?!.*\()/i;
     const toNumberList = args => args.split(",").map(parseFloat);
+    const apply = (fn, args) => {};
 };
 /* --- END HELPER FUNCTIONS --- */
 
