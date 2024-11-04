cc5109203ad6673006e0c46d7659066f40586953
karlchvojka <karl.chvojka@gmail.com>
2024-11-03 19:50:53 -0500

    IMP: FCC/JS/17_SpreadSheet: Lesson 41

--- FreeCodeCamp/JS_Algor_Data_Structures/17_SpreadSheet/script.js
+++ FreeCodeCamp/JS_Algor_Data_Structures/17_SpreadSheet/script.js
@@ -22,6 +22,7 @@
     const evalFormula = (x, cells) => {
         const idToText = (id) => cells.find((cell) => cell.id === id).value;
         const rangeRegex = /([A-J])([1-9][0-9]?):([A-J])([1-9][0-9]?)/gi;
+        const rangeFromString = (num1, num2) => range(parseInt(num1), parseInt(num2));
     };
 
     /**
