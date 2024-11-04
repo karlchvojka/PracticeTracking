8313eea683d61fed1cf473a555e86aa0c72f9d01
karlchvojka <karl.chvojka@gmail.com>
2024-11-03 20:21:55 -0500

    IMP: FCC/JS/17_SpreadSheet: Lesson 53

--- FreeCodeCamp/JS_Algor_Data_Structures/17_SpreadSheet/script.js
+++ FreeCodeCamp/JS_Algor_Data_Structures/17_SpreadSheet/script.js
@@ -25,6 +25,7 @@
         const rangeFromString = (num1, num2) => range(parseInt(num1), parseInt(num2));
         const elemValue = (num) => character => idToText(character + num);
         const addCharacters = character1 => character2 => num => charRange(character1, character2).map(elemValue(num));
+        const rangeExpanded = x.replace(rangeRegex);
     };
 
     /**
