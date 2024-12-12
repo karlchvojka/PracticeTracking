3dc31c96be7851874335b67e5795cef7418dc84a
karlchvojka <karl.chvojka@protonmail.com>
2024-12-12 14:14:30 -0500

    DOC: Added commenting and additions from FCC

---
+++ FreeCodeCamp/JS_Algor_Data_Structures/18_Phone_Number_Validator/script.js
@@ -0,0 +1,21 @@
+/**
+ * Phone Number Validation Functions
+ *
+ * Functions for handeling validation of phone numbers
+ */
+
+/* --- LOCAL DATA --- */
+/* --- END LOCAL DATA --- */
+
+/* --- VARIABLE DECARATIONS --- */
+/* --- END VARIABLE DECLARATIONS --- */
+
+/* --- CUSTOM EVENT HANDLERS --- */
+/* --- END CUSTOM EVENT HANDLERS --- */
+
+/* --- GLOBAL FUNCTIONS --- */
+/* --- END GLOBAL FUNCTIONS --- */
+
+/* --- INTERFACE FUNCTIONS --- */
+/* --- END INTERFACE FUNCTIONS --- */
+
