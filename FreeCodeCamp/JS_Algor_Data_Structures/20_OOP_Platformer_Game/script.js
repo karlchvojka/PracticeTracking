c253381b7d96f88372ba8e879e8611b19987e7c9
karlchvojka <karl.chvojka@gmail.com>
2025-03-23 17:42:57 -0400

    IMP: FCC/JS/20_OOP_Platformer_Game: Lesson 43

--- FreeCodeCamp/JS_Algor_Data_Structures/20_OOP_Platformer_Game/script.js
+++ FreeCodeCamp/JS_Algor_Data_Structures/20_OOP_Platformer_Game/script.js
@@ -65,6 +65,7 @@ const player = new Player();
 const animate = () => {
     requestAnimationFrame(animate);
     ctx.clearRect(0, 0, canvas.width, canvas.height);
+    player.update();
 };
 
 const startGame = () => {
