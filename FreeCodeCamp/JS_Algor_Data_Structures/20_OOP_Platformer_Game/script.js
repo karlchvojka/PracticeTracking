61df867d2e1743f1203d34138d6867589228c12c
karlchvojka <karl.chvojka@gmail.com>
2025-03-23 16:02:02 -0400

    IMP: FCC/JS/20_OOP_Platformer_Game: Lesson 4

--- FreeCodeCamp/JS_Algor_Data_Structures/20_OOP_Platformer_Game/script.js
+++ FreeCodeCamp/JS_Algor_Data_Structures/20_OOP_Platformer_Game/script.js
@@ -3,3 +3,4 @@ const checkpointScreen = document.querySelector(".checkpoint-screen");
 const startBtn = document.getElementById("start-btn");
 const startScreen = document.querySelector(".start-screen");
 const checkpointMessage = document.querySelector(".checkpoint-screen > p");
+const ctx = canvas.getContext("2d");
