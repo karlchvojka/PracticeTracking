d9ee245af71777f2a2835bfb7194f4078134211e
karlchvojka <karl.chvojka@gmail.com>
2025-03-23 19:55:54 -0400

    IMP: FCC/JS/20_OOP_Platformer_Game: Lesson 92

--- FreeCodeCamp/JS_Algor_Data_Structures/20_OOP_Platformer_Game/script.js
+++ FreeCodeCamp/JS_Algor_Data_Structures/20_OOP_Platformer_Game/script.js
@@ -142,7 +142,10 @@ const animate = () => {
             player.position.y <= platform.position.y + platform.height,
         ];
 
-        if (platformDetectionRules.every(rule => rule)) {};
+        if (platformDetectionRules.every(rule => rule)) {
+            player.position.y = platform.position.y + player.height;
+            player.velocity.y = gravity;
+        };
     });
 };
 
