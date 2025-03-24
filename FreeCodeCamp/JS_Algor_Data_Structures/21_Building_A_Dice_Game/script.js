5a4d94ba574cca1ee8edbf9a9c50e4c2fa09812c
karlchvojka <karl.chvojka@gmail.com>
2025-03-24 19:29:41 -0400

    IMP: FCC/JS/21_Building_Dice_Game : 04

--- FreeCodeCamp/JS_Algor_Data_Structures/21_Building_A_Dice_Game/script.js
+++ FreeCodeCamp/JS_Algor_Data_Structures/21_Building_A_Dice_Game/script.js
@@ -19,15 +19,28 @@ let score = 0;
 let total = 0;
 let round = 1;
 
-rollDiceBtn.addEventListener("click", () => {
-    const randomNum = () => Math.floor(Math.random()*6 + 1);
-
+const rollDice = () => {
+    diceValuesArr = [];
+    
     for (let i = 0; i < 5; i++) {
-        diceValuesArr[i] = randomNum();
-        listOfAllDice[i].textContent = `${diceValuesArr[i]}`;
+        const randomDice = Math.floor(Math.random() * 6) + 1;
+        diceValuesArr.push(randomDice);
     }
-});
 
+    listOfAllDice.forEach((dice, index) => {
+        dice.textContent = diceValuesArr[index]
+    });
+};
+
+rollDiceBtn.addEventListener("click", () => {
+    if (rolls === 3) {
+        alert("Maximum Rolls Reached - Select a score");
+    } else {
+        rollDice();
+        rolls++;
+    }
+
+});
 
 rulesBtn.addEventListener("click", () => {
     isModalShowing = !isModalShowing;
