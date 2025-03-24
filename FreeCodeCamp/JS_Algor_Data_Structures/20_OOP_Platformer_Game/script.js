37a9eb52aa612ad9862b64348b61023a9d96ebf0
karlchvojka <karl.chvojka@gmail.com>
2025-03-23 20:33:50 -0400

    IMP: FCC/JS/20_OOP_Platformer_Game: Lesson 104

--- FreeCodeCamp/JS_Algor_Data_Structures/20_OOP_Platformer_Game/script.js
+++ FreeCodeCamp/JS_Algor_Data_Structures/20_OOP_Platformer_Game/script.js
@@ -157,10 +157,15 @@ const animate = () => {
 
         checkpoints.forEach((checkpoint) => {
             checkpoint.position.x -= 5;
-        })
+        });
+
         } else if (keys.leftKey.pressed && isCheckpointCollisionDetectionActive) {
             platforms.forEach((platform) => {
                 platform.position.x += 5;
+            });
+            
+            checkpoints.forEach((checkpoint) => {
+                checkpoint.position.x += 5;
             });
         }
     }
