78bb58ee1f4c8792fa648bf0f76dba30eb05f669
karlchvojka <karl.chvojka@gmail.com>
2025-03-25 21:05:57 -0400

    IMP: FCC/JS/22_Cash_Register: Purchase button event listener

--- FreeCodeCamp/JS_Algor_Data_Structures/22_Cash_Register/script.js
+++ FreeCodeCamp/JS_Algor_Data_Structures/22_Cash_Register/script.js
@@ -1,4 +1,8 @@
-let price = 1.87;
+/**
+ * FCC Cash Register Project
+ */
+
+/* --- LOCAL DATA --- */
 let cid = [
     ['PENNY', 1.01],
     ['NICKEL', 2.05],
@@ -10,3 +14,21 @@ let cid = [
     ['TWENTY', 60],
     ['ONE HUNDRED', 100]
 ];
+let price = 1.87;
+/* --- END LOCAL DATA --- */
+
+/* --- INTERFACE VARIABLES --- */
+const cash = document.getElementById("cash");
+const changeDue = document.querySelector("#change-due > p");
+const purchaseBtn = document.getElementById("purchase-btn");
+/* --- END INTERFACE VARIABLES --- */
+
+/* --- EVENT HANDLERS --- */
+purchaseBtn.addEventListener("click", () => {
+    if(cash.value < price) {
+        alert("Customer does not have enough money to purchase the item");
+    } else if (cash.value = price) {
+        changeDue.innerHTML = "No change due - customer paid with exact cash"
+    }
+});
+/* --- END EVENT HANDLERS --- */
