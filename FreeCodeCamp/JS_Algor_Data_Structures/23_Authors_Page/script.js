d9957042c3878e36c934958b5a69965fe166bad3
karlchvojka <karl.chvojka@gmail.com>
2025-03-26 18:27:16 -0400

    IMP: FCC/JS/23_Authors_Page: Step 15

--- FreeCodeCamp/JS_Algor_Data_Structures/23_Authors_Page/script.js
+++ FreeCodeCamp/JS_Algor_Data_Structures/23_Authors_Page/script.js
@@ -21,6 +21,7 @@ fetch('https://cdn.freecodecamp.org/curriculum/news-author-page/authors.json')
     .then((res) => res.json())
     .then((data) => {
         authorDataArr = data;
+        console.log('Author Data Array:', authorDataArr);
     })
     .catch((err) => {
         console.log(`There was an error: ${err}`);
