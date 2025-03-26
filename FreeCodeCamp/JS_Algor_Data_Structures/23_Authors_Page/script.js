3240d54eebf8bc8824d23d09732fa216fb660387
karlchvojka <karl.chvojka@gmail.com>
2025-03-26 18:10:44 -0400

    IMP: FCC/JS/23_Authors_Page: Step 7

--- FreeCodeCamp/JS_Algor_Data_Structures/23_Authors_Page/script.js
+++ FreeCodeCamp/JS_Algor_Data_Structures/23_Authors_Page/script.js
@@ -10,14 +10,21 @@ const loadMoreBtn = document.getElementById("load-more-btn");
 /* --- END INTERFACE VARIABLES --- */
 
 /* --- LOCAL DATA --- */
+let authorDataArr = [];
+let endingIndex = 8;
+let startingIndex = 0;
+
+/**
+ * API Call to the FCC API
+ */
 fetch('https://cdn.freecodecamp.org/curriculum/news-author-page/authors.json')
     .then((res) => res.json())
     .then((data) => {
         console.log(data);
     })
     .catch((err) => {
-        consol.elog(`There was an error: ${err}`);
-    }
+        console.log(`There was an error: ${err}`);
+    });
 
 /* --- END LOCAL DATA --- */
 
