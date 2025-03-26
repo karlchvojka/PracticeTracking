394767c82fe31b4c4927329be10a1f4fbc5a51f4
karlchvojka <karl.chvojka@gmail.com>
2025-03-26 17:46:14 -0400

    IMP: FCC/JS/23_Authors_Page: Step 1

---
+++ FreeCodeCamp/JS_Algor_Data_Structures/23_Authors_Page/script.js
@@ -0,0 +1,10 @@
+/**
+ * Author Page
+ *
+ * freeCodeCamp Author Page Project Script
+ */
+
+/* --- INTERFACE VARIABLES --- */
+const authorContainer = document.getElementById("authorContainer");
+const loadMoreBtn = document.getElementById("load-more-btn");
+/* --- END INTERFACE VARIABLES --- */
