97c9c44b27ed7a2c9210a2f17ad5b9bb7efa1d07
karlchvojka <karl.chvojka@gmail.com>
2025-03-27 15:30:27 -0400

    IMP: FCC/JS/24_Forum_Leaderboard: Step 2

--- FreeCodeCamp/JS_Algor_Data_Structures/24_Forum_Leaderboard/script.js
+++ FreeCodeCamp/JS_Algor_Data_Structures/24_Forum_Leaderboard/script.js
@@ -5,6 +5,8 @@
  */
 
 /* --- LOCAL DATA --- */
+const avatarUrl = "https://sea1.discourse-cdn.com/freecodecamp";
+const forumCategoryUrl = "https://forum.freecodecamp.org/c/";
 const forumLatest = "https://cdn.freecodecamp.org/curriculum/forum-latest/latest.json";
 const forumTopicUrl = "https://forum.freecodecamp.org/t/";
 /* --- END LOCAL DATA --- */
