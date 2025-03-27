3b2b5aa6abc165592866093f8c19d7123cc92cdd
karlchvojka <karl.chvojka@gmail.com>
2025-03-27 18:27:06 -0400

    IMP: FCC/JS/24_Forum_Leaderboard: Step 36

--- FreeCodeCamp/JS_Algor_Data_Structures/24_Forum_Leaderboard/script.js
+++ FreeCodeCamp/JS_Algor_Data_Structures/24_Forum_Leaderboard/script.js
@@ -41,6 +41,7 @@ const forumCategory = (id) => {
         const { className, category } = allCategories[id];
         selectedCategory.className = className;
         selectedCategory.category = category;
+    } else {
     }
 }
 
