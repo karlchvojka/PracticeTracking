5804d64fac9dfd0ae406ce65bc98d5051b8d7d4d
karlchvojka <karl.chvojka@gmail.com>
2025-03-27 16:33:20 -0400

    IMP: FCC/JS/24_Forum_Leaderboard: Step 15

--- FreeCodeCamp/JS_Algor_Data_Structures/24_Forum_Leaderboard/script.js
+++ FreeCodeCamp/JS_Algor_Data_Structures/24_Forum_Leaderboard/script.js
@@ -45,6 +45,16 @@ const showLatestPosts = (data) => {
     const { topic_list, users } = data;
     const { topics } = topic_list;
 
-    postsContainer.innerHTML = topics.map((item) => {});
+    postsContainer.innerHTML = topics.map((item) => {
+        const { 
+            id, 
+            title,
+            views,
+            posts_count,
+            slug, posters,
+            category_id,
+            bumped_at 
+        } = item;
+    });
 };
 /* --- END GLOBAL FUNCTIONS --- */
