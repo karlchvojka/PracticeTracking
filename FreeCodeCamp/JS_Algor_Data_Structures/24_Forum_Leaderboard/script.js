ceb396d74c23b7ca55d0384784649e3bb6232a6a
karlchvojka <karl.chvojka@gmail.com>
2025-03-27 15:56:05 -0400

    IMP: FCC/JS/24_Forum_Leaderboard: Step 9

--- FreeCodeCamp/JS_Algor_Data_Structures/24_Forum_Leaderboard/script.js
+++ FreeCodeCamp/JS_Algor_Data_Structures/24_Forum_Leaderboard/script.js
@@ -26,8 +26,8 @@ const fetchData = async () => {
     try {
         const res = await fetch(forumLatest);
         const data = await res.json();
-        console.log(data)
     } catch (err) {
+        console.log(err);
     }
 };
 
