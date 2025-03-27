cb53ed56eb264ff76e199e05425511401c1775b5
karlchvojka <karl.chvojka@gmail.com>
2025-03-27 18:46:42 -0400

    IMP: FCC/JS/24_Forum_Leaderboard: Step 42

--- FreeCodeCamp/JS_Algor_Data_Structures/24_Forum_Leaderboard/script.js
+++ FreeCodeCamp/JS_Algor_Data_Structures/24_Forum_Leaderboard/script.js
@@ -51,7 +51,7 @@ const forumCategory = (id) => {
     const linkText = selectedCategory.category;
     const linkClass = `category ${selectedCategory.className}`;
 
-    return `<a href="${url}"></a>`;
+    return `<a href="${url}" class="${linkClass}" target="_blank">${linkText}</a>`;
 };
 
 /**
