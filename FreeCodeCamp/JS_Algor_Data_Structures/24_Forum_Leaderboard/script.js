3dcc1dfab045562b2d294281851a5e716c06121b
karlchvojka <karl.chvojka@gmail.com>
2025-03-27 15:28:04 -0400

    IMP: FCC/JS/24_Forum_Leaderboard: Step 1

---
+++ FreeCodeCamp/JS_Algor_Data_Structures/24_Forum_Leaderboard/script.js
@@ -0,0 +1,10 @@
+/**
+ * freeCodeCamp Forum Leaderboard
+ *
+ * Leaderboard project using API data from the FCC forum.
+ */
+
+/* --- LOCAL DATA --- */
+const forumLatest = "https://cdn.freecodecamp.org/curriculum/forum-latest/latest.json";
+const forumTopicUrl = "https://forum.freecodecamp.org/t/";
+/* --- END LOCAL DATA --- */
