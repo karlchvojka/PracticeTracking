3bee61f346f1216a27f050e13feaa97e2dbdfe48
karlchvojka <karl.chvojka@gmail.com>
2025-03-27 18:14:11 -0400

    IMP: FCC/JS/24_Forum_Leaderboard: Finishing allCategories definition

--- FreeCodeCamp/JS_Algor_Data_Structures/24_Forum_Leaderboard/script.js
+++ FreeCodeCamp/JS_Algor_Data_Structures/24_Forum_Leaderboard/script.js
@@ -8,7 +8,12 @@
 const allCategories = {
     299: { category: 'Career Advice', className: "career" },
     409: { category: "Project Feedback", className: "feedback" },
-    417: { category: "freeCodeCamp Support", className: "support" }
+    417: { category: "freeCodeCamp Support", className: "support" },
+    421: { category: "JavaScript", className: "javascript" },
+    423: { category: "HTML - CSS", className: "html-css" },
+    424: { category: "Python", className: "python" },
+    432: { category: "You Can Do This!", className: "motivation" },
+    560: { category: "Backend Development", className: "backend" }
 };
 const avatarUrl = "https://sea1.discourse-cdn.com/freecodecamp";
 const forumCategoryUrl = "https://forum.freecodecamp.org/c/";
