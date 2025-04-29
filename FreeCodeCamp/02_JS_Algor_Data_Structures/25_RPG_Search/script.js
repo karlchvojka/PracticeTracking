87cd551760ea5b142d25157d13b52ae1283da27a
karlchvojka <karl.chvojka@gmail.com>
2025-04-29 15:06:09 -0400

    DOCS: Added base structure to scripts.js

---
+++ FreeCodeCamp/02_JS_Algor_Data_Structures/25_RPG_Search/script.js
@@ -0,0 +1,14 @@
+/**
+ * FreeCodeCamp RPG Creature Search
+ * 
+ * A simple application to allow searching for RPG characters from an API
+ */
+
+/* --- LOCAL DATA --- */
+/* --- END LOCAL DATA --- */
+
+/* --- INTERFACE VARIABLES --- */
+/* --- END INTERFACE VARIABLES --- */
+
+/* --- GLOBAL FUNCTIONS --- */
+/* --- END GLOBAL FUNCTIONS --- */
