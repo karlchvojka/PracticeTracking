eb3fa1081a5ef1d103a829b4b8d60ca7c615c8a0
karlchvojka <karl.chvojka@gmail.com>
2025-05-27 02:18:48 -0400

    IMP: Lesson 37 - Completed

---
+++ FreeCodeCamp/03_Front_End_Dev_Libraries/04_React/37/index.js
@@ -0,0 +1,19 @@
+/**
+ * FreeCodeCamp Lesson 37
+ *
+ * Answer for Lesson 37
+ */
+
+const styles = {
+  color: "purple",
+  fontSize: 40,
+  border: "2px solid purple"
+};
+
+class Colorful extends React.component {
+  render() {
+    return (
+      <div style={styles}>Style Me!</div>
+    );
+  }
+}
