67c54b2c8425cf022af2330c8287d290b56aa2ad
karlchvojka <karl.chvojka@gmail.com>
2025-05-27 02:08:41 -0400

    IMP: Lesson 36 - Completed

---
+++ FreeCodeCamp/03_Front_End_Dev_Libraries/04_React/36/index.js
@@ -0,0 +1,13 @@
+/**
+ * FreeCodeCamp Lesson 36
+ * 
+ * Answer for lesson 36
+ */
+
+class Colorful extends React.Component {
+  render() {
+    return (
+      <div style={{color: "red", fontSize: 72 }} >Big Red</div>
+    );
+  }
+}
